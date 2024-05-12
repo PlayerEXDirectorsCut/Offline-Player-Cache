ed5bc94d532ba20152906519bdab0f211ca6b756
Bibi Reden <redwitblue@gmail.com>
2024-05-12 17:53:07 -0500

    Update definitions

--- src/client/kotlin/com/bibireden/opc/OfflinePlayerCacheClient.kt
+++ src/client/kotlin/com/bibireden/opc/OfflinePlayerCacheClient.kt
@@ -2,7 +2,7 @@ package com.bibireden.opc
 
 import net.fabricmc.api.ClientModInitializer
 
-object OfflineplayercacheClient : ClientModInitializer {
+object OfflinePlayerCacheClient : ClientModInitializer {
 	override fun onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
--- src/main/kotlin/com/bibireden/opc/OfflinePlayerCache.kt
+++ src/main/kotlin/com/bibireden/opc/OfflinePlayerCache.kt
@@ -3,7 +3,7 @@ package com.bibireden.opc
 import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
-object Offlineplayercache : ModInitializer {
+object OfflinePlayerCache : ModInitializer {
     private val logger = LoggerFactory.getLogger("offline-player-cache")
 
 	override fun onInitialize() {
